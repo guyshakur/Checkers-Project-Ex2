185976e8e4019936c7aed9257db3db5d0d684305
guy schiff <guyshakur@gmail.com>
2020-08-31 08:38:19 +0300

    updated the project and have 2 classes.checkers model (the board model with all the logic) and the ui right now is "Game" but will change

---
+++ C20 Ex02 Guy 200118602 Elior 203136916/Checkers/CheckersModel.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Checkers
+{
+
+    class CheckersModel
+    {
+        private char[,] board = null;
+
+        private int initialFilledRowsForPlayer = 0;
+        private int InitialFilledRowsForPlayer
+        {
+            get
+            {
+                return initialFilledRowsForPlayer;
+            }
+
+            set
+            {
+                if (value == 10 || value == 8 || value == 6)
+                {
+                    initialFilledRowsForPlayer = value;
+                }
+
+                else
+                {
+                    initialFilledRowsForPlayer = 10;
+                }
+            }
+        }
+        public CheckersModel(int boardSize)
+        {
+            board = new char[boardSize, boardSize];
+
+            initialFilledRowsForPlayer = (boardSize - 2) / 2;
+
+            bool fillEvenCells = true;
+
+            for (int y = 0; y < boardSize; y++)
+            {
+                for (int x = 0; x < boardSize; x++)
+                {
+                    board[x, y] = ' ';
+                }
+            }
+
+            for (int y = 0; y < initialFilledRowsForPlayer; y++)
+            {
+                fillEvenCells = (((y + 1) % 2) == 0);
+                for (int x = 0; x < boardSize; x++)
+                {
+                    if (fillEvenCells)
+                    {
+                        board[x, y] = (x % 2 == 0) ? 'O' : ' ';
+                    }
+                    else
+                    {
+                        board[x, y] = (x % 2 == 0) ? ' ' : 'O';
+                    }
+                }
+            }
+
+
+            for (int y = boardSize - 1; y > boardSize - initialFilledRowsForPlayer - 1; y--)
+            {
+                fillEvenCells = (boardSize - 1 - y) % 2 == 0;
+                for (int x = 0; x < boardSize; x++)
+                {
+                    if (fillEvenCells)
+                    {
+                        board[x, y] = (x % 2 == 0) ? 'X' : ' ';
+                    }
+                    else
+                    {
+                        board[x, y] = (x % 2 == 0) ? ' ' : 'X';
+                    }
+                }
+            }
+
+
+        }
+
+        public char getCell(int x, int y)
+        {
+
+            return board[x, y];
+        }
+
+
+
+    }
+}
---
+++ C20 Ex02 Guy 200118602 Elior 203136916/Checkers/Game.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ex02.ConsoleUtils;
+
+
+namespace Checkers
+{
+    class Game
+    {
+        private static string playerName;
+        private static int boardSize;
+        private static int numOfPlayers;
+
+        public static void Main()
+        {
+
+            Console.WriteLine("         Checkers Game");
+            Console.WriteLine();
+
+            getUserInitialInput();
+            Screen.Clear();
+
+            CheckersModel model = new CheckersModel(boardSize);
+            string boardHeader = "   A   B   C   D   E   F   G   H   I   J";
+            Console.WriteLine(" " + boardHeader.Substring(0, boardSize * 4));
+            Console.WriteLine("  " + new String('=', boardSize * 4 + 1));
+
+            string boardSideHeader = "abcdefghij";
+            for (int y = 0; y < boardSize; y++)
+            {
+                Console.Write(boardSideHeader.Substring(y, 1) + " ");
+                Console.Write("|");
+                for (int x = 0; x < boardSize; x++)
+                {
+
+                    Console.Write(" " + model.getCell(x, y) + " ");
+                    Console.Write("|");
+                }
+                Console.WriteLine();
+                Console.WriteLine("  " + new String('=', boardSize * 4 + 1));
+            }
+
+
+        }
+
+        private static int getIntFromUser(string msg, params int[] validInputs)
+        {
+            int result = 0;
+            bool validNum = false;
+            while (!validNum)
+            {
+                Console.Write(msg);
+                string numAsStr = Console.ReadLine();
+
+                if (Int32.TryParse(numAsStr, out result))
+                {
+                    foreach (int validInput in validInputs)
+                    {
+                        if (result == validInput)
+                        {
+                            validNum = true;
+                        }
+                    }
+
+                }
+            }
+            return result;
+        }
+
+        public static void getUserInitialInput()
+        {
+
+
+            playerName = getStringFromUser("Enter your name (20 characters and no spaces): ");
+
+
+            boardSize = getIntFromUser("Choose board size (6,8,10): ", 6, 8, 10);
+
+            numOfPlayers = getIntFromUser("Enter number of players (1,2): ", 1, 2);
+
+        }
+        private static string getStringFromUser(string msg)
+        {
+            string result = "";
+            bool validStr = false;
+            while (!validStr)
+            {
+                Console.Write(msg);
+                result = Console.ReadLine();
+                if (!(result.Contains(' ')) && result.Length <= 20)
+                {
+                    validStr = true;
+                }
+
+            }
+            return result;
+        }
+    }
+}
